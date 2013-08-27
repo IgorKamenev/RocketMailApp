3f1beb3f726939335ce372bb5a429c6f29c4baac
Igor Kamenev <igorkamenev@yandex.ru>
2013-08-27 19:48:29 +0400

    initial commit

---
+++ RocketMailApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RocketMailApp
+//
+//  Created by Igor Kamenev on 8/27/13.
+//  Copyright (c) 2013 Igor Kamenev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ RocketMailApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RocketMailApp
+//
+//  Created by Igor Kamenev on 8/27/13.
+//  Copyright (c) 2013 Igor Kamenev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
